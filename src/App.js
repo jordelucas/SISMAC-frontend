a8a47b24cdc9bffb773eac356bfaaafcc54edf20
jordeva.silva <jordevalucas@hotmail.com>
2020-03-22 11:12:02 -0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import './globals.css'
+import './Navbar.css'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello word</h1>
+    <div id="app">
+
+      <header className="navbar">
+        <span className="brand">SISMAC</span>
+        
+        <div className="user">
+          <div className="welcome">
+            <small>Bem-vindo(a)</small>
+            <strong>Usuário</strong>
+          </div>
+        
+          <img 
+            src="https://avatars2.githubusercontent.com/u/33496399?s=460&u=ba659f521711486b87e6f5e9ea7f19b770c7fa0c&v=4"
+            alt="Foto do perfil"
+          />
+        </div>
+      </header>
+      
+      <main>
+        <h1>Main</h1>
+      </main>
+      
+      <footer>
+        <h1>Footer</h1>
+      </footer>
     </div>
   );
 }
