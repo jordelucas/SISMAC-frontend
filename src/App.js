1336765d326dc53ab930bb86d5e938fb20d7a899
jordeva.silva <jordevalucas@hotmail.com>
2020-03-22 15:06:55 -0300

    change - main for component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
 import React from 'react';
 
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-
 import './App.css'
-import './Main.css'
 
 import GlobalStyle from './styles/global'
-import Footer from './components/footer'
 import Navbar from './components/navbar'
+import Main from './components/main'
+import Footer from './components/footer'
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      
       <div id="app">
         <Navbar />
-        
-        <main>
-          <ul className="options">
-            <li>
-              <button>
-                <EventNoteIcon style={{ fontSize: 50 }}/>
-                <span>Agendamentos</span>
-              </button>
-            </li>
-            <li>
-              <button>
-                <LocalHospitalIcon style={{ fontSize: 50 }}/>
-                <span>Consultas e procedimentos</span>
-              </button>
-            </li>
-            <li>
-              <button>
-                <PeopleAltIcon style={{ fontSize: 50 }}/>
-                <span>Usuários</span>
-              </button>
-            </li>
-          </ul>
-        </main>
-        
+        <Main />
         <Footer />
       </div>
     </>
---
+++ src/components/main/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+
+import StyledMain from './styled'
+
+function Main() {
+    return (
+        <StyledMain>
+            <ul>
+                <li>
+                    <button>
+                        <EventNoteIcon style={{ fontSize: 50 }}/>
+                        <span>Agendamentos</span>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <LocalHospitalIcon style={{ fontSize: 50 }}/>
+                        <span>Consultas e procedimentos</span>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <PeopleAltIcon style={{ fontSize: 50 }}/>
+                        <span>Usuários</span>
+                    </button>
+                </li>
+          </ul>
+        </StyledMain>  
+    )
+}
+
+export default Main;
