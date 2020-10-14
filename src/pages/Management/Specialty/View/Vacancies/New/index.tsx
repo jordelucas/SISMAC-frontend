33bb0024e7f6b470c61df8aef04c6a3cde61b827
jordelucas <jordevalucas@gmail.com>
2020-10-13 23:11:08 -0300

    integra cadastro de vagas para especialidade

---
+++ src/pages/Management/Specialty/View/Vacancies/New/index.tsx
@@ -0,0 +1,115 @@
+import React, { FormEvent, useEffect, useState } from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+import api from '../../../../../../services/api';
+
+import Content from '../../../../../../components/Layout/Content';
+import Wrapper from '../../../../../../components/Layout/Wrapper';
+import BackButton from '../../../../../../components/BackButton';
+import Title from '../../../../../../components/Title';
+import { Button } from '../../../../../../components/Button';
+import Input from '../../../../../../components/Input';
+
+import { 
+  Form, 
+  FormGroup, 
+  Grid
+} from './styles';
+
+interface LocationState {
+  id: {
+    pathname: string;
+  };
+}
+
+interface SpecialtyProps {
+  nomeEspecialidade: string;
+}
+
+const NewVacancies: React.FC = () => {
+  const { state: specialtyId } = useLocation<LocationState>();
+  
+  const [nomeEspecialidade, setNomeEspecialidade] = useState('')
+  const [data, setData] = useState('')
+  const [vagas, setVagas] = useState('')
+
+  const { id } = specialtyId || { id: { pathname: "/" } };
+
+  useEffect(() => {
+    api.get<SpecialtyProps>(
+      `especialidades/${id}`
+    ).then((response) => {
+      const { 
+        nomeEspecialidade: specialtyNome,
+       } = response.data;
+
+       setNomeEspecialidade(specialtyNome)
+    }) 
+  }, [id])
+
+  function handleCreateVacancy(e: FormEvent) {
+    e.preventDefault();
+
+    api.post('vagas', {
+      data,
+      vagasOfertadas: vagas,
+      especialidade: id,
+      exame: null,
+      consulta: true,
+    }).then(() => {
+      alert('Cadastro realizado com sucesso!')
+      clearStates();
+    }).catch(() => {
+      alert('Erro no cadastro!')
+    })
+  }
+
+  function clearStates(){
+    setData('')
+    setVagas('')
+  }
+
+  return (
+    <Content>
+      <Wrapper>
+        <>
+          <BackButton link={{
+            pathname: `/management/specialty/${id}`,
+            state: {
+              id,
+            }
+          }}/>
+          <Title text={`Novas vagas - ${nomeEspecialidade}`} />
+
+          <Form onSubmit={handleCreateVacancy}>
+            <Grid>
+              <FormGroup gridArea='DT'>
+                <Input 
+                  type="date"
+                  label="Data" 
+                  identifier="data"
+                  value={data}
+                  onChange={setData}
+                  disabled={false}/>
+              </FormGroup>
+              <FormGroup gridArea='VG'>
+                <Input 
+                  type="number"
+                  label="Quantidade de vagas"
+                  identifier="vagas"
+                  value={vagas}
+                  onChange={setVagas}
+                  disabled={false}/>
+              </FormGroup>
+            </Grid>
+
+            <Button type="submit">Salvar</Button>
+          </Form>
+        </>
+      </Wrapper>
+    </Content>
+  );
+}
+
+export default NewVacancies;
